2bd11bd2df32fd04c0024eac304e8a48f15d513e
Shah Bano <whatsmyusername82@gmail.com>
2025-06-15 02:42:32 -0400

    m

--- src/App.js
+++ src/App.js
@@ -118,7 +118,7 @@ export default function WorldMap() {
                   height="1"
                 >
                   <image
-                    src={`${process.env.PUBLIC_URL}/images/${image}`}
+                    href={`${process.env.PUBLIC_URL}/images/${image}`}
                     width="100"
                     height="100"
                     preserveAspectRatio="xMidYMid slice"
